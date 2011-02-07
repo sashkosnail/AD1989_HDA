ad7b91c9cea5965f56cfba1eadcfdaa035563395
Eurotech <sashkosnail@gmail.com>
2011-02-07 14:32:34 -0500

    Script correction

--- sh/sync_misc.sh
+++ sh/sync_misc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dir_list="DEBUG archive wav_sample test graphs"
+dir_list="DEBUG archive wav_sample test graphs tmp"
 for i in $dir_list; do
 	if [ -d $i ]; then
 		scp user@129.100.33.18:~/alsa_mod/$i/* ./$i/		
